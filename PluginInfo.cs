327d47525e996272b00a58b7fb81c3a98c2014f1
myepikcooldude <113408854+myepikcooldude@users.noreply.github.com>
2025-01-09 21:09:23 -0600

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "org.bruhvr.gorillatag.bruhvrmenu";
+        public const string Name = "Bruh V1";
+        public const string Description = "first menu!!!";
+        public const string Version = "1.0.0";
+    }
+}
